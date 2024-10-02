e63f8857c0990b55c59bf99eb0f80a5a2d14856f
Kalisom Cruz <kalisom.cruz@vumer.com.br>
2024-10-01 21:06:03 -0300

    Att 01.10 21:05

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const ejs = require('ejs')
 const http = require('http')
 
-const routes = require('./routes/routes')
+const general = require('./routes/general')
 
 var port = 50553 //80: Porta padrão HTTP | 443: Porta padrão HTTPS
 
@@ -12,7 +12,7 @@ const server = http.createServer(app)
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname+'/public'))
 
-app.use('/', routes)
+app.use('/', general)
 
 server.listen(port, () => {
     console.log('Servidor rodando')
