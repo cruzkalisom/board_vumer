cf758d466891ec67381ed6ecc0e208ecddca5692
Kalisom Cruz <kalisom.cruz@vumer.com.br>
2024-10-11 22:11:31 -0300

    Att 11.10 22:11h

---
+++ public/js/general/login.js
@@ -0,0 +1,65 @@
+var alert = document.getElementById('alert')
+
+$("#button-submit").on('click', (event) => {
+    event.preventDefault()
+
+    alert.hidden = true
+
+    var email = document.getElementById('email')
+    var password = document.getElementById('password')
+    var remember = document.getElementById('remember')
+
+    email.classList.remove('is-invalid')
+    password.classList.remove('is-invalid')
+
+    var auth = true
+    var message = ''
+
+    if(password.value.length == 0){
+        password.classList.add('is-invalid')
+        auth = false
+        message = 'Senha Inválida'
+    }
+
+    if(!(/^[^\s@]+@[^\s@]+\.[^\s@]+$/).test(email.value)){
+        email.classList.add('is-invalid')
+        auth = false
+        message = 'E-mail inválido'
+    }
+
+    if(!auth){
+        return alerta(message) 
+    }
+
+    var formData = new FormData()
+    var url = '/login'
+
+    formData.append('email', email.value)
+    formData.append('password', password.value)
+    formData.append('remember', remember.checked)
+
+    $.ajax({
+        url: url,
+        method: 'POST',
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: (data) => {
+            alerta(data.status)
+        },
+        error: (err) => {
+            console.error('Erro ao solicitar resposta na rota ' + url)
+            console.error(err)
+        }
+    })
+})
+
+function alerta (message){
+    alert.innerHTML = `
+        <div class="alert alert-danger alert-dimissible" role="alert">
+            <div>${message}</div>
+        </div>
+    `
+
+    alert.hidden = false
+}
--- routes/general.js
+++ routes/general.js
@@ -3,7 +3,31 @@ const uploads = require('../modules/uploads')
 const db = require('../modules/db')
 
 router.post('/login', uploads.any(), (req, res) => {
-    console.log(req.body)
+    var sql = 'SELECT * FROM users WHERE email = ?'
+
+    if((!req.body.email || req.body.email == undefined) || (!req.body.password || req.body.password == undefined)){
+        return res.json({invalid: true})
+    }
+
+    db.query(sql, [req.body.email], (err, result) => {
+        if(err){
+            return console.error(err.message)
+        }
+
+        if(!result[0]){
+            return res.json({invalidEmail: true})
+        }
+
+        if(req.body.password !== result[0].password){
+            return res.json({invalidPassword: true})
+        }
+
+        res.json({status: true})
+    })
+})
+
+router.get('/login', (req, res) => {
+    res.render('general/login')
 })
 
 router.post('/register', uploads.any(), (req, res) => {
