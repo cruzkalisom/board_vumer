09a3c23910c4db6ca9c7ca867fa50a92b3448b38
arthurr7070 <arthurscherer1644@gmail.com>
2024-12-02 22:02:18 -0300

    02 12 2024  22:02

--- public/js/admin/admin.js
+++ public/js/admin/admin.js
@@ -7,6 +7,9 @@ $.ajax({
         if(data.notlogin){
             return location.href = '/login'
         }
+        if(data.status){
+            document.getElementById('username').innerText = data.name
+        }
     },
     error: (err) => {
         console.error('Erro ao solicitar resposta na rota ' + urlAdmin)
--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ const db = require('../modules/db')
 
 router.post('/', (req, res) => {
     var sql = 'SELECT * FROM sessions WHERE token = ? AND user_id = ?'
+    var sql2 = 'SELECT * FROM users WHERE user_id = ?'
 
     if((!req.session.token || req.session.token == undefined) || (!req.session.user_id || req.session.user_id == undefined)){
         return res.json({notlogin: true})
@@ -20,12 +21,31 @@ router.post('/', (req, res) => {
             return res.json({notlogin: true})
         }
 
-        res.json({status: true})
+        db.query(sql2, [user_id], (err, result) => {
+            if(err){
+                return console.error(err.message)
+            }  
+
+            if(!result[0]){
+                return res.json({notlogin: true})
+            }
+
+            var name = `${result[0].name} ${result[0].surname}`
+
+            var dataToSend = {
+                status: true,
+                name: name,
+            }
+
+            res.json(dataToSend)
+        })
     })
 })
 
 router.get('/', (req, res) => {
+    req.session.oldpage = '/admin'
     res.render('admin/index')
 })
+
 
 module.exports = router
