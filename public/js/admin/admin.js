d1a00c793abf5deb197764ebb8932e67ff99defc
arthurr7070 <arthurscherer1644@gmail.com>
2024-12-11 21:15:10 -0300

    11 12 2024 21 15

--- public/js/admin/admin.js
+++ public/js/admin/admin.js
@@ -17,6 +17,43 @@ $.ajax({
     }
 })
 
+$('#create-game').on('click', () => {
+    var gameType = document.getElementById('create-game-type')
+    var gameName = document.getElementById('create-game-name')
+    var gameDescription = document.getElementById('create-game-description')
+    var url = '/admin/create-game'
+    var formData = new FormData()
+    
+    if(!(/[a-zA-Z]/).test(gameName.value)){
+        return alert('Digite um nome para o jogo')
+    }
+
+    formData.append('gameName', gameName.value)
+    formData.append('gameType', gameType.value)
+    formData.append('gameDescription', gameDescription.value)
+
+    $.ajax({
+        url: url,
+        method: 'POST',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: (data) => {
+            console.log(data)
+        },
+        error: (err) => {
+            console.error('Erro ao solicitar resposta na rota ' + url)
+            console.error(err)
+        }
+
+
+    })
+
+
+})
+
 $('#new-game').on('click', (event) => {
     event.preventDefault()
 })
+
+
